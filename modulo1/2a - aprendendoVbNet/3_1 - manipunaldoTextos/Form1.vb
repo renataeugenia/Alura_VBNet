c9d710dd6d46b51b3cb69473f15b6021a01f3ac2
renataeugenia <eugeniacomercial@gmail.com>
2022-07-15 15:53:23 -0300

    nova solução

---
+++ modulo1/2a - aprendendoVbNet/3_1 - manipunaldoTextos/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        MsgBox("Manipulando textos...")
+
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
